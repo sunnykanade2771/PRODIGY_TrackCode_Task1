1e0f3053bcec3a4a5534f86029f18a31aa6524a7
sunnykanade2771 <sunnyskanade@gmail.com>
2026-01-04 21:00:46 +0530

    Add script.js

---
+++ script.js
@@ -0,0 +1,9 @@
+const navbar = document.getElementById("navbar");
+
+window.addEventListener("scroll", () => {
+  if (window.scrollY > 80) {
+    navbar.classList.add("scrolled");
+  } else {
+    navbar.classList.remove("scrolled");
+  }
+});
